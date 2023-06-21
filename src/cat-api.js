f0a465901b5b47f094204ba0b823158cf028f8d4
AmaJuli <ostapchukyuliiaa@gmail.com>
2023-06-21 09:41:57 +0300

    add

---
+++ src/cat-api.js
@@ -0,0 +1,28 @@
+const URL = 'https://api.thecatapi.com/v1/';
+
+const options = {
+  headers: {
+    'x-api-key':
+      'live_LVTjDkbBC8d8WUnhtAkzQftUgDngEnavyUVEoaQixFEc0HQBqpu8EIbo5ZdggVqr',
+  },
+};
+
+function fetchBreeds(url) {
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
+
+function fetchCatByBreed(url) {
+  return fetch(url, options).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
+
+export { URL, fetchBreeds, fetchCatByBreed };
